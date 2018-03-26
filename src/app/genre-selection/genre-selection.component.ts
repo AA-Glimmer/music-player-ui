9a573cb0c94d6c89f8e1622c9e491383e83c4d4a
Amit Prasad <amitprasad53121@gmail.com>
2018-03-26 01:50:38 +0100

    api redirection to homepage from genre selection completed

--- src/app/genre-selection/genre-selection.component.ts
+++ src/app/genre-selection/genre-selection.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {SessionService} from '../services/session.service';
 
 @Component({
   selector: 'app-genre-selection',
@@ -7,16 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreSelectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router, private sessionService: SessionService) {
+  }
 
   orderableList = ['Jazz', 'Hip Hop', 'Rock'];
+  username = null;
 
   ngOnInit() {
-
+    const sesssion = this.sessionService.getSession();
+    // Check if user is in session
+    if (sesssion === undefined || !('user' in sesssion)) {
+      // if not then redirect to login page
+      this.router.navigate(['./login']);
+    } else {
+      this.username = sesssion['user']['username'];
+    }
   }
 
 
   confirmGenreOrdering() {
     console.log(this.orderableList);
+
+    let promise = new Promise((resolve, reject) => {
+      this.http.post(this.baseUrl + '/genreselection', {
+        username: this.username,
+        genreSelection: this.orderableList,
+      })
+        .toPromise()
+        .then(
+          data => {
+            // console.log('Response ' + data);
+            console.log('Login Success');
+            this.router.navigate(['./homepage']);
+            resolve();
+          },
+          err => {
+            // Error
+            console.log('Error status: ' + err.status);
+            alert(err.msg);
+          }
+        );
+    });
   }
 }
